92d1cc8f6facc81254d9b5e1033cab80cb47e39d
Hadar Pur <30519609+HadarPur@users.noreply.github.com>
2022-03-04 00:22:38 +0200

    Add files via upload

---
+++ attack/attacker.py
@@ -0,0 +1,20 @@
+from locust.log import setup_logging
+from locust.env import Environment
+from locust.runners import STATE_SPAWNING, STATE_RUNNING, STATE_CLEANUP
+from locust import HttpUser, between, task, User
+
+class Attacker(object):
+    def __init__(self, user: Type[User], count: int, spawn_rate: int) -> None:
+        # setup Environment and Runner
+        self.env = Environment(user_classes=[user])
+        self.env.create_local_runner()
+        self.count = count
+        self.spawn_rate = spawn_rate
+
+    def start(self):
+        # start the test
+        self.env.runner.start(self.count, spawn_rate=self.spawn_rate)
+
+    def stop(self):
+        self.env.runner.quit()
+        self.env.runner.greenlet.join()
