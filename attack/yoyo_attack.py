315afd546956ce952c6858ab4663f8aa5f56b2c9
Hadar Pur <30519609+HadarPur@users.noreply.github.com>
2022-03-04 19:52:16 +0200

    Update yoyo_attack.py

--- attack/yoyo_attack.py
+++ attack/yoyo_attack.py
@@ -1,6 +1,6 @@
-from locust.log import setup_logging
-from .yoyo import YoYo
 import pyfiglet
+from locust.log import setup_logging
+from yoyo import YoYo
 
 title = pyfiglet.figlet_format("DDOS YoYo Attack", font="bulbhead")
 
@@ -8,7 +8,7 @@ TARGET_AUTOSCALERS = ['service-a-autoscaler','service-b-autoscaler','service-c-a
 TARGET_SERVICES = ['service-a','service-b','service-c','service-d']
 
 if __name__ == '__main__':
+    print(title)
     setup_logging("INFO", None)
     yoyo = YoYo(TARGET_AUTOSCALERS, TARGET_SERVICES)
-    print(title)
     yoyo.start()
