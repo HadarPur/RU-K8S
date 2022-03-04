3c42918795910fe9461b3f42ed14e9f557df6401
Hadar Pur <30519609+HadarPur@users.noreply.github.com>
2022-03-04 03:52:00 +0200

    Update yoyo_attack.py

--- attack/yoyo_attack.py
+++ attack/yoyo_attack.py
@@ -1,6 +1,9 @@
 import numpy as np
 import os
 import logging
+import pyfiglet
+  
+title = pyfiglet.figlet_format("DDOS YoYo Attack", font = "digital" )
 
 TARGET_AUTOSCALERS = ['service-a-autoscaler','service-b-autoscaler','service-c-autoscaler','service-d-autoscaler']
 TARGET_SERVICES = ['service-a','service-b','service-c','service-d']
@@ -8,4 +11,5 @@ TARGET_SERVICES = ['service-a','service-b','service-c','service-d']
 if __name__ == '__main__':
     setup_logging("INFO", None)
     yoyo = YoYo(TARGET_AUTOSCALERS, TARGET_SERVICES)
+    print(title)
     yoyo.start()
