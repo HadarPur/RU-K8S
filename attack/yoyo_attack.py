32982c3e472fe074c69e6345fc856fe07cc35fcc
Hadar Pur <30519609+HadarPur@users.noreply.github.com>
2022-03-05 19:01:41 +0200

    Add files via upload

---
+++ attack/yoyo_attack.py
@@ -0,0 +1,14 @@
+import pyfiglet
+from locust.log import setup_logging
+from yoyo import YoYo
+
+title = pyfiglet.figlet_format("DDOS YoYo Attack")
+
+TARGET_AUTOSCALERS = ['service-a-autoscaler','service-b-autoscaler','service-c-autoscaler','service-d-autoscaler']
+TARGET_SERVICES = ['service-a','service-b','service-c','service-d']
+
+if __name__ == '__main__':
+    print(title)
+    setup_logging("INFO", None)
+    yoyo = YoYo(TARGET_AUTOSCALERS, TARGET_SERVICES)
+    yoyo.start()
